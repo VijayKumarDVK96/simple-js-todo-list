4b65db97236aa9849371be38dfad9ec317fff4c9
VijayKumarDVK96 <43247564+VijayKumarDVK96@users.noreply.github.com>
2022-11-04 11:11:55 +0530

    edit function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,21 +3,24 @@
 const formEl = document.getElementById('form-item');
 const itemEl = document.getElementById('item');
 const itemContainerEl = document.getElementById('item-container');
+let btnSubmit = document.getElementById('btn-submit');
 
 let items = [];
+let isEditing = false;
+let editId = 0;
 
-const displayUI = function() {
-    if(items.length > 0) {
+const displayUI = function () {
+    if (items.length > 0) {
         itemContainerEl.innerHTML = '';
 
         items.forEach((item) => {
             const listEl = document.createElement('li');
             listEl.classList.add('list-item');
-            listEl.innerHTML = `${item.value} <button onclick='deleteItem(${item.id})'>Delete</button>`;
+            listEl.innerHTML = `${item.value} <button onclick='editItem(${item.id})'>Edit</button><button onclick='deleteItem(${item.id})'>Delete</button>`;
 
             itemContainerEl.appendChild(listEl);
         });
-        
+
     }
 };
 
@@ -27,23 +30,46 @@ const deleteItem = function (id) {
     displayUI();
 }
 
-formEl.addEventListener('submit', function(e) {
+const editItem = function (id) {
+    const itemToEdit = items.find((item) => item.id === id);
+    itemEl.value = itemToEdit.value;
+    editId = id;
+    isEditing = true;
+    displayUI();
+
+    btnSubmit.value = 'Update';
+}
+
+formEl.addEventListener('submit', function (e) {
     e.preventDefault();
+    if (itemEl.value) {
+        if (isEditing) {
+            items = items.map((item) => {
+                if (item.id === editId) {
+                    return {...item, value: itemEl.value};
+                } else {
+                    return item;
+                }
+            });
 
-    if(itemEl.value) {
-        // const item = itemEl.value;
-        // items.push(item);
-        // itemEl.value = null;
+            editId = null;
+            isEditing = false;
+            itemEl.value = null;
 
-        const item = {
-            id: new Date().valueOf(),
-            value: itemEl.value
+            displayUI();
+
+            btnSubmit.value = 'Add';
+        } else {
+            const item = {
+                id: new Date().valueOf(),
+                value: itemEl.value
+            }
+            items.push(item);
+            itemEl.value = null;
+
+            // console.log(items);
+            displayUI();
         }
-        items.push(item);
-        itemEl.value = null;
-
-        // console.log(items);
-        displayUI();
     } else {
         alert('Enter a valid input');
     }
